41d4f61173bf2767605886da39363329955e9b04
zlodag <11688686+zlodag@users.noreply.github.com>
2021-08-08 17:09:23 +1200

    code generation

--- db/connectors.ts
+++ db/connectors.ts
@@ -8,15 +8,15 @@ export const getAllGroups = () => pool
   .query('SELECT id, name FROM "group" ORDER BY id')
   .then(result => result.rows);
 
-export const getMembersOfGroup = (groupId: number) => pool
-  .query('SELECT u.id, u.name FROM "user" u INNER JOIN user_group x ON x.user=u.id AND x.group=$1 ORDER BY u.id', [groupId])
+export const getMembersOfGroup = (groupId: string) => pool
+  .query('SELECT u.id, u.name, u.created FROM "user" u INNER JOIN user_group x ON x.user=u.id AND x.group=$1 ORDER BY u.id', [groupId])
   .then(result => result.rows);
 
-export const getGroupsForUser = (userId: number) => pool
+export const getGroupsForUser = (userId: string) => pool
   .query('SELECT g.id, g.name FROM "group" g INNER JOIN user_group x ON x.group=g.id AND x.user=$1 ORDER BY g.id', [userId])
   .then(result => result.rows);
 
-export const getUser = (id: number) => pool
+export const getUser = (id: string) => pool
   .query('SELECT id, name, created FROM "user" WHERE id=$1', [id])
   .then(result => result.rows[0]);
 
