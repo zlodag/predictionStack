c7acf33e263f4fbfe9ceb4a8d8abdb249e1eda0b
zlodag <11688686+zlodag@users.noreply.github.com>
2021-08-10 22:20:22 +1200

    cors

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import {graphqlHTTP} from 'express-graphql';
 import {schema} from './schema';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:8000',
+}));
 
 app.use('/graphql', graphqlHTTP({
   schema,
